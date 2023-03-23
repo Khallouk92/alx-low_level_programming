2f48836eeac20e94f78f9c75f9c4e8e9651ed404
khallouk92 <khallouk1992y@gmail.com>
2023-03-22 19:50:53 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
-	#include "main.h"
+#include "main.h"
 
 
-	/**
-	 * print_to_98 - prints all natural numbers from n to 98,
-	 * followed by a new line
-	 * @n: print from this number
-	 */
-	void print_to_98(int n)
-	{
-		int i, j;
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
+int i, j;
 
-
-		if (n <= 98)
-		{
-			for (i = n; i <= 98; i++)
-			{
-				if (i != 98)
-					printf("%d, ", i);
-				else if (i == 98)
-					printf("%d\n", i);
-			}
-		} else if (n >= 98)
-		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
-		}
-	}
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+{
+_putchar(i + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(i + '0');
+_putchar('\n');
+}
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+{
+_putchar(j + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(j + '0');
+_putchar('\n');
+}
+}
+}
+}
